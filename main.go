2c82f0136a63f92c5bbf822d4a2f72e830fab403
Jørgen Navjord <navjordj@gmail.com>
2019-09-03 18:20:53 +0200

    work on post

--- main.go
+++ main.go
@@ -3,13 +3,14 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/gorilla/mux"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 type person struct {
 	name string
-	age int
+	age  int
 }
 
 func (p *person) birthday() {
@@ -22,14 +23,36 @@ func (p *person) ageInTenYears() int {
 
 func getPerson(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-type", "application/json")
-	json.NewEncoder(w).Encode("walla brusjan")
+	json.NewEncoder(w).Encode("hmm")
 }
 
+func addPerson(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method == "POST" {
+		w.Header().Set("Content-type", "application/json")
+
+		//var newPerson person
+
+		for k, v := range mux.Vars(r) {
+			fmt.Print(k, v)
+		}
+	}
+
+}
+
+var Personer []person
+
 func main() {
-	//kristian := person{"Jørgen", 30}
+
+	per := person{"Per", 30}
+	per.birthday()
+
+	Personer = append(Personer, per)
+
 	fmt.Println("Starter serveren")
 	r := mux.NewRouter()
 	r.HandleFunc("/person", getPerson).Methods(("GET"))
+	r.HandleFunc("/person", getPerson).Methods(("POST"))
 	http.ListenAndServe(":8000", r)
 	fmt.Println("Lytter nå på port 8000")
 }
